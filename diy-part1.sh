e9794e7774f16dcdde26c794fdebf2370ff4eaa2
try496 <66765024+try496@users.noreply.github.com>
2022-11-12 16:10:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,26 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git ssrp https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/Gzxhwq/openwrt-passwall;packages-me' feeds.conf.default
+#sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' feeds.conf.default
+#sed -i '$a src-git passwall_luci https://github.com/Gzxhwq/openwrt-passwall;luci-me' feeds.conf.default
+sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
+sed -i '$a src-git passwall2 https://github.com/Gzxhwq/openwrt-passwall2;private' feeds.conf.default
+#sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+#sed -i '1i src-git mosdns https://github.com/QiuSimons/openwrt-mos' feeds.conf.default
+
+#Add external packages
+#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/others/luci-app-openclash
+#git submodule add https://github.com/destan19/OpenAppFilter.git package/others/OpenAppFilter
+#git submodule add https://github.com/tty228/luci-app-serverchan.git package/others/luci-app-serverchan
+#git submodule add -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/others/luci-theme-edge
+#git submodule add https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/others/luci-theme-infinityfreedom
+#git submodule add https://github.com/garypang13/luci-app-eqos.git package/others/luci-app-eqos
+#git submodule add https://github.com/lisaac/luci-app-dockerman.git package/others/luci-app-dockerman
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/others/luci-app-amlogic
+git clone https://github.com/thinktip/luci-theme-neobird.git package/others/luci-theme-neobird
+svn co https://github.com/QiuSimons/openwrt-mos/trunk/v2ray-geodata package/others/v2ray-geodata
