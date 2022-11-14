6ae758d93d59dcedc94f43cdf997e84e089904a7
try496 <66765024+try496@users.noreply.github.com>
2022-11-14 18:22:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo'src-git helloworld https://github.com/fw876/helloworld'>>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo“src-git small https://github.com/kenzok8/small”>>feeds.conf.default
-echo“src-git others https://github.com/kenzok8/openwrt-packages”>>feeds.conf.default
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
+sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
