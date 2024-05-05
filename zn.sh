63480f0045f6a4312d347f517da2a72d1d554d9d
He Ping <pinghejk@gmail.com>
2024-05-05 08:53:48 +0800

    更新 openwrt.sh

--- zn.sh
+++ zn.sh
@@ -12,7 +12,7 @@ function merge_package(){
     git sparse-checkout set "$@"
     mv -f "$@" "$rootdir"/"$localdir" && cd "$rootdir"
 }
-merge_package main https://github.com/try496/openwrt-packages luci-app-cpufreq cpufreq luci-app-zerotier luci-app-msd_lite msd_lite luci-app-ddns-go ddns-go
+merge_package main https://github.com/try496/openwrt-packages luci-app-cpufreq cpufreq luci-app-zerotier luci-app-ddns-go ddns-go
 
 # wechatpush
 git clone https://github.com/tty228/luci-app-wechatpush.git package/apps/luci-app-wechatpush
@@ -28,6 +28,7 @@ git clone https://github.com/tty228/luci-app-wechatpush.git package/apps/luci-ap
 
 # 时间服务器
 sed -i 's/0.openwrt.pool.ntp.org/cn.ntp.org.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
 sed -i 's/2.openwrt.pool.ntp.org/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
 sed -i 's/3.openwrt.pool.ntp.org/ntp.tencent.com/g' package/base-files/files/bin/config_generate
 
@@ -41,7 +42,7 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 sed -i '/root/c\root:$1$3INQuMmE$eyGe2r1bt96nb2Oqm.oaQ1:19563:0:99999:7:::' package/base-files/files/etc/shadow
 
 # 修改IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 # 修改IPv6 端口号
 sed -i 's/\[::\]:80/\[::\]:8060/g' package/network/services/uhttpd/files/uhttpd.config
